b54480bef8f45c3934be128177b1ce4e774d6ad7
João Vitor Gonçalves <joao_vitor_araki_goncalves@hotmail.com>
2018-11-01 22:13:41 -0300

    max incovenience ongoing

--- methods/tripmethods.py
+++ methods/tripmethods.py
@@ -1,5 +1,36 @@
 from functools import reduce
 
+
+def calculate_max_incovenience_ongoing(ongoing_trip, starting_trip, dist):
+    (origin_a, dest_a, current_a) = ongoing_trip
+    (origin_b, dest_b) = starting_trip
+
+    base_time_a = dist[current_a][dest_a]
+    base_time_b = dist[origin_b][dest_b]
+
+    possible_paths = []
+
+    # A,X,C,B,D
+    path = (origin_a, current_a, origin_b, dest_a, dest_b)
+    incovenience_a = (dist[current_a][origin_b] +
+                      dist[origin_b][dest_a]) / base_time_a
+    incovenience_b = (dist[origin_b][dest_a] +
+                      dist[dest_a][dest_b]) / base_time_b
+    possible_paths.append((path, max(incovenience_a, incovenience_b)))
+
+    # A,X,C,D,B
+    path = (origin_a, current_a, origin_b, dest_b, dest_a)
+    incovenience_a = (dist[current_a][origin_b] +
+                      dist[origin_b][dest_b] +
+                      dist[dest_b][dest_a]) / base_time_a
+    incovenience_b = 1
+    possible_paths.append((path, max(incovenience_a, incovenience_b)))
+
+    result = reduce((lambda a, b: a if a[1] < b[1] else b), possible_paths)
+
+    return result
+
+
 def calculate_max_incovenience(trip_a, trip_b, dist):
     (origin_a, dest_a) = trip_a
     (origin_b, dest_b) = trip_b
@@ -19,8 +50,9 @@ def calculate_max_incovenience(trip_a, trip_b, dist):
 
     # A C D B
     path = (origin_a, origin_b, dest_b, dest_a)
-    incovenience_a = (dist[origin_a][origin_b] + dist[origin_b]
-                      [dest_b] + dist[dest_b][dest_a]) / base_time_a
+    incovenience_a = (dist[origin_a][origin_b] +
+                      dist[origin_b][dest_b] +
+                      dist[dest_b][dest_a]) / base_time_a
     incovenience_b = 1
     possible_paths.append((path, max(incovenience_a, incovenience_b)))
 
@@ -35,8 +67,9 @@ def calculate_max_incovenience(trip_a, trip_b, dist):
     # C A B D
     path = (origin_b, origin_a, dest_a, dest_b)
     incovenience_a = 1
-    incovenience_b = (dist[origin_b][origin_a] + dist[origin_a]
-                      [dest_a] + dist[dest_a][dest_b]) / base_time_b
+    incovenience_b = (dist[origin_b][origin_a] +
+                      dist[origin_a][dest_a] +
+                      dist[dest_a][dest_b]) / base_time_b
     possible_paths.append((path, max(incovenience_a, incovenience_b)))
 
     result = reduce((lambda a, b: a if a[1] < b[1] else b), possible_paths)
